c1de47d4e27d44748b0780a336d75de5b3b6399a
Gregor Remec <remecgregor@gmail.com>
2024-10-20 10:48:12 -0600

    build expandable text and nav bar routing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo3.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
       <HStack padding="10px">
-        <Image src={logo} boxSize={"75px"}></Image>
+        <Link to={"/"}>
+          <Image src={logo} boxSize={"75px"} minWidth={"80px"}></Image>
+        </Link>
+
         <SearchInput />
         <ColorModeSwitch />
       </HStack>
