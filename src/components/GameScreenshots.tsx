243d07fe2a67804e89ed7ed8ddfebf044462782f
Gregor Remec <remecgregor@gmail.com>
2024-10-21 09:13:49 -0600

    build game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,24 @@
+import useScreenshots from "../hooks/useScreenshots";
+import { SimpleGrid, Image } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  return (
+    <SimpleGrid spacing={2} columns={{ base: 1, md: 2 }}>
+      {data?.results.map((file) => (
+        <Image src={file.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
